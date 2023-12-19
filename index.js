b38fca9dc55bf246e537c5161de917f097b9f14f
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-18 20:55:40 -0500

    Lab completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) +1
+
+
+let num3 = 44
+let num4 = 10
+let mod = (num3 % num4)
+
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
